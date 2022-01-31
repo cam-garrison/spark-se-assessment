39f6577b5b1b8f2f9a1a5e8406c8b2a00a4378e2
Cameron Garrison <cgarriso@bu.edu>
2022-01-31 17:31:04 -0500

    trying new thing in launch

--- launch.sh
+++ launch.sh
@@ -5,5 +5,6 @@ export APP_SETTINGS="project.server.config.DevelopmentConfig"
 # flask db init
 # flask db migrate
 flask db upgrade
+python project/server/__init__.py db upgrade
 #flask run --host=0.0.0.0 --port=5000
 # flask run
